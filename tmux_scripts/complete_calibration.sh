7f301ea9f76fb72885df9fa3a015a74fefc44a74
Myralllka <Olli25010@gmail.com>
2022-03-24 13:41:26 +0100

    fixed calibration

--- tmux_scripts/complete_calibration.sh
+++ tmux_scripts/complete_calibration.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-rostopic pub -1 "/$UAV_NAME/basler_stereo_driver/$UAV_NAME/complete std_msgs/"Bool True
+rostopic pub -1 "/$UAV_NAME/basler_stereo_driver/$UAV_NAME/complete" std_msgs/Bool True
